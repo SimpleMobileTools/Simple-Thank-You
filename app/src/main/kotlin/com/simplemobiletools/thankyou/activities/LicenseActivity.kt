7dd27f51b93d64d3a6e34014a45d8b27fb98645b
tibbi <tibor@kaputa.sk>
2016-12-09 20:48:39 +0100

    add License activity content

--- app/src/main/kotlin/com/simplemobiletools/thankyou/activities/LicenseActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/thankyou/activities/LicenseActivity.kt
@@ -1,9 +1,22 @@
 package com.simplemobiletools.thankyou.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import com.simplemobiletools.thankyou.R
+import kotlinx.android.synthetic.main.activity_license.*
 
 class LicenseActivity : SimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license)
+
+        license_kotlin_title.setOnClickListener { openUrl(R.string.kotlin_url) }
+    }
+
+    private fun openUrl(id: Int) {
+        val url = resources.getString(id)
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
     }
 }
