edfdf79e977bf4bbe4faf58235687738affbffc8
tibbi <tibor@kaputa.sk>
2016-12-09 20:04:30 +0100

    add kotlin

---
+++ app/src/main/kotlin/com/simplemobiletools/thankyou/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.simplemobiletools.thankyou.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.simplemobiletools.thankyou.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
