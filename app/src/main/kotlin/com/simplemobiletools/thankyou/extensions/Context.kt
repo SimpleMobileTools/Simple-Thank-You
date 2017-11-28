935d7d17b097671eb2b9d5708df280df1f08f152
tibbi <tibor@kaputa.sk>
2017-11-28 19:08:48 +0100

    updating commons, gradle, Android Studio etc

--- app/src/main/kotlin/com/simplemobiletools/thankyou/extensions/Context.kt
+++ app/src/main/kotlin/com/simplemobiletools/thankyou/extensions/Context.kt
@@ -3,4 +3,4 @@ package com.simplemobiletools.thankyou.extensions
 import android.content.Context
 import com.simplemobiletools.thankyou.helpers.Config
 
-val Context.config: Config get() = Config.newInstance(this)
+val Context.config: Config get() = Config.newInstance(applicationContext)
