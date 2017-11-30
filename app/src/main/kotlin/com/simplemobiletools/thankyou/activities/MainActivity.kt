9d5c90520707fcc06653a2d7650349c9e7a07249
tibbi <tibor@kaputa.sk>
2017-11-30 20:32:57 +0100

    adding a toggle for forcing english

--- app/src/main/kotlin/com/simplemobiletools/thankyou/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/thankyou/activities/MainActivity.kt
@@ -6,25 +6,39 @@ import android.view.Menu
 import android.view.MenuItem
 import com.simplemobiletools.commons.extensions.appLaunched
 import com.simplemobiletools.commons.extensions.checkWhatsNew
+import com.simplemobiletools.commons.extensions.restartActivity
 import com.simplemobiletools.commons.extensions.updateTextColors
 import com.simplemobiletools.commons.helpers.LICENSE_KOTLIN
 import com.simplemobiletools.commons.models.Release
 import com.simplemobiletools.thankyou.BuildConfig
 import com.simplemobiletools.thankyou.R
+import com.simplemobiletools.thankyou.extensions.config
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : SimpleActivity() {
+    var storedUseEnglish = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         appLaunched()
         checkWhatsNewDialog()
+        storeStateVariables()
     }
 
     override fun onResume() {
         super.onResume()
+        if (storedUseEnglish != config.useEnglish) {
+            restartActivity()
+            return
+        }
+
         updateTextColors(activity_main)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        storeStateVariables()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -39,6 +53,12 @@ class MainActivity : SimpleActivity() {
             else -> return super.onOptionsItemSelected(item)
         }
         return true
+    }
+
+    private fun storeStateVariables() {
+        config.apply {
+            storedUseEnglish = useEnglish
+        }
     }
 
     private fun launchSettings() {
--- app/src/main/kotlin/com/simplemobiletools/thankyou/activities/SettingsActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/thankyou/activities/SettingsActivity.kt
@@ -4,10 +4,13 @@ import android.content.ComponentName
 import android.content.pm.PackageManager
 import android.os.Bundle
 import com.simplemobiletools.commons.dialogs.ConfirmationDialog
+import com.simplemobiletools.commons.extensions.beVisibleIf
 import com.simplemobiletools.commons.extensions.updateTextColors
+import com.simplemobiletools.commons.extensions.useEnglishToggled
 import com.simplemobiletools.thankyou.R
 import com.simplemobiletools.thankyou.extensions.config
 import kotlinx.android.synthetic.main.activity_settings.*
+import java.util.*
 
 class SettingsActivity : SimpleActivity() {
 
@@ -20,6 +23,7 @@ class SettingsActivity : SimpleActivity() {
         super.onResume()
 
         setupCustomizeColors()
+        setupUseEnglish()
         setupHideLauncherIcon()
         updateTextColors(settings_holder)
     }
@@ -27,6 +31,16 @@ class SettingsActivity : SimpleActivity() {
     private fun setupCustomizeColors() {
         settings_customize_colors_holder.setOnClickListener {
             startCustomizationActivity()
+        }
+    }
+
+    private fun setupUseEnglish() {
+        settings_use_english_holder.beVisibleIf(config.wasUseEnglishToggled || Locale.getDefault().language != "en")
+        settings_use_english.isChecked = config.useEnglish
+        settings_use_english_holder.setOnClickListener {
+            settings_use_english.toggle()
+            config.useEnglish = settings_use_english.isChecked
+            useEnglishToggled()
         }
     }
 
