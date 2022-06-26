46ebee83e18af684537f6e6cc478f3bd006dc656
tibbi <tibor@kaputa.sk>
2022-06-26 21:48:27 +0200

    adding a flag for the fdroid build variant

--- app/src/main/kotlin/com/simplemobiletools/thankyou/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/thankyou/activities/MainActivity.kt
@@ -49,10 +49,13 @@ class MainActivity : SimpleActivity() {
 
     private fun launchAbout() {
         val faqItems = arrayListOf(
-            FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
-            FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons),
             FAQItem(R.string.faq_7_title_commons, R.string.faq_7_text_commons)
         )
+
+        if (!resources.getBoolean(R.bool.hide_google_relations)) {
+            faqItems.add(FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons))
+            faqItems.add(FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons))
+        }
 
         startAboutActivity(R.string.app_name, 0, BuildConfig.VERSION_NAME, faqItems, false)
     }
